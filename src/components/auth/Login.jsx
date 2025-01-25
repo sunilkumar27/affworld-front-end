cb2c5733fe0f60d38fffa5d6f924addb7335e2dc
sunilkumar27 <sunilkumar27@gmail.com>
2025-01-25 07:59:40 +0530

    Added import

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,5 @@
 // src/components/auth/Login.jsx
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/auth/AuthContext';
 import { useAsync } from '../../hooks/useAsync';
