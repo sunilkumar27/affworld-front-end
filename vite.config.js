17f8e5d18acd55f32a76de3f58ab64bca51d9b98
sunilkumar27 <sunilkumar27@gmail.com>
2025-01-25 12:36:59 +0530

    Added missing import

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
